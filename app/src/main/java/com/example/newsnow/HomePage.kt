d1650e2ebf214c8376397d732a10bf223fcaa36e
noureddine <bounarda1234@gmail.com>
2024-11-27 19:22:07 +0100

    news app

--- app/src/main/java/com/example/newsnow/HomePage.kt
+++ app/src/main/java/com/example/newsnow/HomePage.kt
@@ -1,19 +1,27 @@
 package com.example.newsnow
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -23,6 +31,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -97,6 +107,10 @@ fun CategoriesBar(newsViewModel: NewsViewModel) {
         mutableStateOf("")
     }
 
+    var isSearchExpanded by remember {
+        mutableStateOf(false)
+    }
+
     val categoriesList = listOf(
         "GENERAL",
         "BUSINESS",
@@ -112,6 +126,36 @@ fun CategoriesBar(newsViewModel: NewsViewModel) {
             .horizontalScroll(rememberScrollState()),
         verticalAlignment = Alignment.CenterVertically
     ) {
+        if (isSearchExpanded) {
+            OutlinedTextField(
+                modifier = Modifier.padding(8.dp)
+                    .height(48.dp)
+                    .border(1.dp, Color.Gray, CircleShape)
+                    .clip(CircleShape),
+                value = searchQuery,
+                onValueChange = {
+                    searchQuery = it
+                },
+                trailingIcon = {
+                    IconButton(onClick = {
+                        isSearchExpanded = false
+                        if(searchQuery.isNotEmpty()){
+                            newsViewModel.fetchEverythingWithQuery(searchQuery)
+                        }
+                    }) {
+                        Icon(imageVector = Icons.Default.Search, contentDescription = "Search icon")
+                    }
+                }
+            )
+        } else {
+            IconButton(onClick = {
+                isSearchExpanded = true
+            }) {
+                Icon(imageVector = Icons.Default.Search, contentDescription = "Search icon")
+            }
+        }
+
+
         categoriesList.forEach { category ->
             Button(
                 onClick = {
